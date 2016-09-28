ad0e93ebfa82e7c44dc895c2cc77d841201afa7c
Lucien Immink <lucien@arielext.org>
2016-09-28 20:13:18 +0200

    [TASK] Update home page, update depencencies

--- js/polyfills-2a526c4c98.js
+++ js/polyfills-2a526c4c98.js
@@ -4930,7 +4930,7 @@ if (typeof define === 'function' && define.amd) {
 })(window, document, 'Hammer');
 
 /*
- * SystemJS v0.19.38
+ * SystemJS v0.19.39
  */
 (function() {
 function bootstrap() {// from https://gist.github.com/Yaffle/1088850
@@ -8259,7 +8259,7 @@ function createEntry() {
       entry.esModule = loader.newModule(getESModule(exports));
     // just use the 'default' export
     else
-      entry.esModule = loader.newModule({ 'default': exports });
+      entry.esModule = loader.newModule({ 'default': exports, __useDefault: true });
   }
 
   /*
@@ -10031,7 +10031,7 @@ hookConstructor(function(constructor) {
 System = new SystemJSLoader();
 
 __global.SystemJS = System;
-System.version = '0.19.38 Standard';
+System.version = '0.19.39 Standard';
   if (typeof module == 'object' && module.exports && typeof exports == 'object')
     module.exports = System;
 
@@ -10046,7 +10046,7 @@ var doPolyfill = typeof Promise === 'undefined';
 if (typeof document !== 'undefined') {
   var scripts = document.getElementsByTagName('script');
   $__curScript = scripts[scripts.length - 1];
-  if ($__curScript.defer || $__curScript.async)
+  if (document.currentScript && ($__curScript.defer || $__curScript.async))
     $__curScript = document.currentScript;
   if (doPolyfill) {
     var curPath = $__curScript.src;
