d0a26e01cd76bd898b6253d110a490a95730482c
Lucien Immink <lucien@arielext.org>
2017-02-09 22:16:38 +0100

    [TASK] Version 2017-02-09 Show file types

--- js/polyfills-576d9cc0c4.js
+++ js/polyfills-576d9cc0c4.js
@@ -5641,7 +5641,7 @@ if (typeof define === 'function' && define.amd) {
 })(window, document, 'Hammer');
 
 /*
- * SystemJS v0.20.5 Dev
+ * SystemJS v0.20.7 Dev
  */
 (function () {
 'use strict';
@@ -5740,6 +5740,7 @@ function throwResolveError () {
   throw new RangeError('Unable to resolve "' + relUrl + '" to ' + parentUrl);
 }
 function resolveIfNotPlain (relUrl, parentUrl) {
+  relUrl = relUrl.trim();
   var parentProtocol = parentUrl && parentUrl.substr(0, parentUrl.indexOf(':') + 1);
 
   var firstChar = relUrl[0];
@@ -6731,11 +6732,15 @@ function doEvaluate (loader, load, link, registry, state, seen) {
       if (module.exports !== moduleObj.default)
         moduleObj.default = module.exports;
 
-      // __esModule flag extension support
-      if (moduleObj.default && moduleObj.default.__esModule)
-        for (var p in moduleObj.default)
-          if (Object.hasOwnProperty.call(moduleObj.default, p) && p !== 'default')
+      // __esModule flag extension support via lifting
+      if (moduleObj.default && moduleObj.default.__esModule) {
+        moduleObj.__useDefault = false;
+        for (var p in moduleObj.default) {
+          if (Object.hasOwnProperty.call(moduleObj.default, p))
             moduleObj[p] = moduleObj.default[p];
+        }
+        moduleObj.__esModule = true;
+      }
     }
   }
 
@@ -6926,7 +6931,8 @@ if (isBrowser) {
       loadingScripts[i].err(msg);
       return;
     }
-    onerror.apply(this, arguments);
+    if (onerror)
+      onerror.apply(this, arguments);
   };
 }
 
@@ -8967,7 +8973,7 @@ function runFetchPipeline (loader, key, metadata, processAnonRegister, wasm) {
     if (!metadata.pluginModule || !metadata.pluginModule.locate)
       return;
 
-    return metadata.pluginModule.locate.call(loader, metadata.pluginLoad)
+    return Promise.resolve(metadata.pluginModule.locate.call(loader, metadata.pluginLoad))
     .then(function (address) {
       if (address)
         metadata.pluginLoad.address = address;
@@ -8979,10 +8985,11 @@ function runFetchPipeline (loader, key, metadata, processAnonRegister, wasm) {
     if (!metadata.pluginModule)
       return fetch$1(key, metadata.load.authorization, metadata.load.integrity, wasm);
 
-    if (!metadata.pluginModule.fetch)
-      return fetch$1(metadata.pluginArgument, metadata.load.authorization, metadata.load.integrity, wasm);
-
     wasm = false;
+
+    if (!metadata.pluginModule.fetch)
+      return fetch$1(metadata.pluginLoad.address, metadata.load.authorization, metadata.load.integrity, false);
+
     return metadata.pluginModule.fetch.call(loader, metadata.pluginLoad, function (load) {
       return fetch$1(load.address, metadata.load.authorization, metadata.load.integrity, false);
     });
@@ -8998,11 +9005,12 @@ function runFetchPipeline (loader, key, metadata, processAnonRegister, wasm) {
     // detect by leading bytes
     if (bytes[0] === 0 && bytes[1] === 97 && bytes[2] === 115) {
       return WebAssembly.compile(bytes).then(function (m) {
-        /* TODO handle imports when `WebAssembly.Module.imports` is implemented
-        if (WebAssembly.Module.imports) {
-          var deps = [];
-          var setters = [];
-          var importObj = {};
+        var deps = [];
+        var setters = [];
+        var importObj = {};
+
+        // we can only set imports if supported (eg Safari doesnt support)
+        if (WebAssembly.Module.imports)
           WebAssembly.Module.imports(m).forEach(function (i) {
             var key = i.module;
             setters.push(function (m) {
@@ -9011,18 +9019,15 @@ function runFetchPipeline (loader, key, metadata, processAnonRegister, wasm) {
             if (deps.indexOf(key) === -1)
               deps.push(key);
           });
-          loader.register(deps, function (_export) {
-            return {
-              setters: setters,
-              execute: function () {
-                _export(new WebAssembly.Instance(m, importObj).exports);
-              }
-            };
-          });
-        }*/
-        // for now we just load WASM without dependencies
-        var wasmModule = new WebAssembly.Instance(m, {});
-        return loader.newModule(wasmModule.exports);
+        loader.register(deps, function (_export) {
+          return {
+            setters: setters,
+            execute: function () {
+              _export(new WebAssembly.Instance(m, importObj).exports);
+            }
+          };
+        });
+        processAnonRegister();
       });
     }
 
@@ -9307,11 +9312,11 @@ function transpile (loader, source, key, metadata, processAnonRegister) {
 
     // translate hooks means this is a transpiler plugin instead of a raw implementation
     if (!transpiler.translate)
-      throw new Error(loader.transpier + ' is not a valid transpiler plugin.');
+      throw new Error(loader.transpiler + ' is not a valid transpiler plugin.');
 
     // if transpiler is the same as the plugin loader, then don't run twice
     if (transpiler === metadata.pluginModule)
-      return load.source;
+      return source;
 
     // convert the source map into an object for transpilation chaining
     if (typeof metadata.load.sourceMap === 'string')
@@ -9588,7 +9593,7 @@ SystemJSLoader$1.prototype.registerDynamic = function (key, deps, executingRequi
   return RegisterLoader$1.prototype.registerDynamic.call(this, key, deps, executingRequire, execute);
 };
 
-SystemJSLoader$1.prototype.version = "0.20.5 Dev";
+SystemJSLoader$1.prototype.version = "0.20.7 Dev";
 
 var System = new SystemJSLoader$1();
 
